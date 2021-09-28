ca1e33f12a81c35e1597c8577c07d9e4e72c1567
Tejas Khatiwala <tejas.khatiwala@gmail.com>
2021-09-28 05:21:36 -0400

    cutils/types: a collection of frequently used C macros

---
+++ libs/cutils/types/include/types.h
@@ -0,0 +1,20 @@
+#ifndef CUTILS_TYPES_H
+#define CUTILS_TYPES_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+#define PTR_TO_ADDR(p) ((uintptr_t)(p))
+#define ADDR_TO_PTR(a) ((void *)(uintptr_t)(a))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CUTILS_TYPES_H
